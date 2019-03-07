0891b588bb937755318cea8779ebb0f502dd4c0d
Mr.Zhang <18301284184@163.com>
2019-03-07 16:25:51 +0800

    服务器部署使用

--- src/main/java/com/bbs/BbsApplication.java
+++ src/main/java/com/bbs/BbsApplication.java
@@ -2,10 +2,16 @@ package com.bbs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BbsApplication {
-
+public class BbsApplication extends SpringBootServletInitializer {
+    //修改入口方法 继承一个SpringBootServletInitializer类，并且覆盖configure方法
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BbsApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(BbsApplication.class, args);
     }
